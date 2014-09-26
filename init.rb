49d00f1a62946d540aaf9b59261b809240f554e3
Danil Tashkinov <danil.tashkinov@gmail.com>
2014-09-26 17:20:41 +0400

    removed warning: remove handler from template name

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :redmine_didyoumean do
   name 'Did You Mean?'
   author 'Alessandro Bahgat and Mattia Tommasone'
   description 'A plugin to search for duplicate issues before opening them.'
-  version '1.2.1'
+  version '1.2.2'
   url 'http://www.github.com/abahgat/redmine_didyoumean'
   author_url 'http://abahgat.com/'
 
